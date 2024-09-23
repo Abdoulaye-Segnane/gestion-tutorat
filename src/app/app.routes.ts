d85241910a0b24f6a95b408353ab180a874a30c1
Abdoulaye <segnanelaye@gmail.com>
2024-09-23 23:13:08 +0000

    Initial commit - mise en place de la partie frontend du projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { SeancesComponent } from './seances/seances.component';
+export const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
+  { path: 'formulaire', component: FormulaireComponent },
+  { path: 'seances', component: SeancesComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Redirection par défaut
+  { path: '**', redirectTo: '/dashboard' } // Redirection en cas de chemin non défini
+];
